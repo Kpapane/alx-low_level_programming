c9eb41955f626f597ae1cf709d6012edfd243173
Katlego Papane <kpapane@gmail.com>
2022-10-13 13:38:23 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main = entry
- * return = always 0
+ * main - Entry
+ * return: always 0
  */
 
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
