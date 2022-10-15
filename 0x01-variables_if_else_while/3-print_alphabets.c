1911b9b47023564a7478bcf3f60a53e21ae3b6aa
Katlego Papane <kpapane@gmail.com>
2022-10-14 18:53:34 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+
+		for (letter = 'A'; letter <= 'Z'; letter++)
+			putchar(letter);
+
+		return (0);
+}
