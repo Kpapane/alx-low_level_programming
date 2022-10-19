ee7cf02020fc40257d915581e7f4b1069e904063
Katlego Papane <kpapane@gmail.com>
2022-10-18 20:33:48 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - prints alphabet
- *
- * Return: 0
  */
 void print_alphabet(void)
 {
-	int alpha;
+	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
-
-	return (0);
 }
