08b0ef02a8500d0d86f9491cba99e52e1967c901
Katlego Papane <kpapane@gmail.com>
2022-10-20 11:57:09 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c: input character
+ * Return: 1 if uppercase, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (10);
+	else
+		return (0);
+}
