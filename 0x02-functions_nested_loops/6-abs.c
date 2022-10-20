d957a5b7b81ec88777f589ca10552cbfa07ad777
Katlego Papane <kpapane@gmail.com>
2022-10-19 19:24:23 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: is an integer
+ * Return: absolute value
+ */
+int _abs(int)
+{
+	int a;
+
+	return (a * ((a > 0) - (a < 0)));
+}
