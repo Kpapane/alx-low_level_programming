03d3b02cf7e88da12b11d70e1ca18505a3fcb291
Katlego Papane <kpapane@gmail.com>
2022-10-19 19:43:12 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: digit
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = (n % 10);
+
+	if (n < 0)
+	{
+		n = (-1 * a);
+	}
+	_putchar(x = '0');
+	return (x);
+}
