51ae634b2b800785dc9326d853c8df356d7ad5d2
Katlego Papane <kpapane@gmail.com>
2022-10-19 18:47:31 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: single inpuut
+ * Returns: 1 for letter, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if  (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
