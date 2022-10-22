74554d859a356dafa632247f2083edac7e59494c
Katlego Papane <kpapane@gmail.com>
2022-10-21 18:50:40 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 
 void print_numbers(void)
 {
-	char num;
+	char c;
 
-	for (num = '0'; num <= '9'; num++)
-		_putchar(num + '0');
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c + '0');
 
 	_putchar('\n');
 }
