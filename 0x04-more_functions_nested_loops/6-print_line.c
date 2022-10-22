97cda4d30697b5c748802961ef8e1ab22b31ec78
Katlego Papane <kpapane@gmail.com>
2022-10-21 19:32:49 -0700

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,9 @@ void print_line(int n)
 {
 	int a;
 
-	if (n > 0)
+	for (a = 0; a < n; a++)
 	{
-		for (a = 1; a <= n; a++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
