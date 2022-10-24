0876c2bd46d9ad9dbbb5896f8dd38ba733b21297
Katlego Papane <kpapane@gmail.com>
2022-10-24 14:54:10 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: a string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int a = 0;
+	int i;
+
+	while (s[a] != '\0')
+		a++;
+	for (i = 0; i < a; i++)
+	{
+		a++;
+		rev = s[i];
+		s[i] = s[a];
+		s[a] = rev;
+	}
+}
