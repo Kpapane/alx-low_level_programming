1adf326170271e0005f44eb9ad8c78240cda893e
Katlego Papane <kpapane@gmail.com>
2022-10-24 15:33:56 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: integer
+ * @n: integer
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0; z < (n - 1); z++)
+	{
+		printf("%d, ", a[z]);
+	}
+	if (z == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
