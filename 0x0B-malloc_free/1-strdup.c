73e24ecb90e3df5be9b57abbe50b8600d49a1140
Katlego Papane <kpapane@gmail.com>
2022-11-09 12:33:41 -0800

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: NULL if str = NULL, returns a pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *strnew = NULL;
+	unsigned int a;
+	int b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		strnew = (char *)malloc(b + 1 * sizeof(char));
+	}
+	if (strnew != NULL)
+	{
+		for (a = 0; str[a] != '\0'; a++)
+			strnew[a] = str[a];
+	}
+	else
+	{
+		return (NULL);
+	}
+	strnew[a] = '\0';
+	return (strnew);
+}
