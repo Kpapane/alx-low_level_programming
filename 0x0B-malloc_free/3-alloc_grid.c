f259bd73af267180e23afb24f7a54351c29a558a
Katlego Papane <kpapane@gmail.com>
2022-11-09 14:04:20 -0800

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: integer value
+ * @height: integer value
+ * Return: NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **matrix, row, column;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	matrix = (int **) malloc(sizeof(int *) * height);
+
+	if (!matrix)
+		return (NULL);
+	for (row = 0; row < height; ++row)
+	{
+		matrix[row] = (int *) malloc(sizeof(int) * width);
+		if (!matrix[row])
+		{
+			while (--row > -1)
+				free(matrix[row]);
+			free(matrix);
+			return (NULL);
+		}
+		for (column = 0; column < width; ++column)
+			matrix[row][column] = 0;
+	}
+	return (matrix);
+}
