b89862c066481f6678ce9deb235b775e2af1acba
Katlego Papane <kpapane@gmail.com>
2022-11-10 12:35:32 -0800

    commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: input one
+ * @s2: input two
+ * @n: number of bytes
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
