543fc9f70db4ae02e0e4e248989e6971dc879009
Katlego Papane <kpapane@gmail.com>
2022-11-15 16:26:35 -0800

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,10 @@ struct dog
 	char *owner;
 };
 
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+typedef struct dog dog_t;
+
+#endif /* DOG_H */
